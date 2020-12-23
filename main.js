d9b7b88411e502c6520f5acd2e7158bb91745ecc
melody31233 <71716855+melody31233@users.noreply.github.com>
2020-12-23 21:42:05 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function showMobileMenu(){
+	let mobilemenu=document.querySelector("#mobile-menu");
+	mobilemenu.style.display="block";
+}// JavaScript Document
+function closeMobileMenu(){
+	let mobilemenu=document.querySelector("#mobile-menu");
+	mobilemenu.style.display="none";
+}
